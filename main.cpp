f713a79fdb4980c0c0618ab62a5a721265429695
attila3453 <attila_geiszt@yahoo.com>
2013-08-21 13:50:13 +0300

    OpenGL Legacy
    Draws a triangle on the screen using OpenGL < 3.3 functions (glBegin,
    glEnd etc.).

--- main.cpp
+++ main.cpp
@@ -1,128 +1,39 @@
 #include <SDL/SDL.h>
-#include <SDL/SDL_main.h>
-#include <SDL/SDL_image.h>
-#include <SDL/SDL_ttf.h>
-#include <SDL/SDL_mixer.h>
-#include <GL/gl.h>
+#include <SDL/SDL_opengl.h>
 #include <stdio.h>
-#include <sstream>
-#include <cstring>
-
-using std::stringstream;
-using std::string;
 
 unsigned int SW = 400;
 unsigned int SH = 400;
-const int FramesPerSecond = 24;
 
-string toString(int nr) {
-    stringstream ss;
-    ss << nr;
-    return ss.str();
-}
+int main(int argc, char **argv){
+    SDL_Init(SDL_INIT_EVERYTHING);
 
-class Timer{
-    private:
-        unsigned int starttime;
-        bool running;
-    public:
-        Timer();
-        void start();
-        void stop();
+    SDL_Window *window = SDL_CreateWindow("SDL Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SW, SH, SDL_WINDOW_OPENGL);
 
-        unsigned int getTime();
-        bool isRunning();
-};
+    SDL_GLContext context = SDL_GL_CreateContext(window);
 
-Timer::Timer(){
-    starttime = 0;
-    running = 0;
-}
+    glMatrixMode(GL_PROJECTION | GL_MODELVIEW);
 
-void Timer::start(){
-    running = 1;
-    starttime = SDL_GetTicks();
-}
+    glLoadIdentity();
 
-void Timer::stop(){
-    running = 0;
-    starttime = 0;
-}
+    glOrtho(0, SW, SH, 0, 0, 1);
 
-unsigned int Timer::getTime(){
-    return SDL_GetTicks() - starttime;
-}
-
-int main(int argc, char ** argv) {
-	SDL_Window* window = 0;
-	SDL_Surface* screenSurface = 0;//pixels
-
-	if(SDL_Init(SDL_INIT_EVERYTHING)){//returns 0 if success
-        printf("SDL error %s", SDL_GetError());
-        return -1;
-	}
-
-	window = SDL_CreateWindow("Crows", 200, 200, SW, SH, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
-
-	if(!window){
-        printf("SDL error %s", SDL_GetError());
-        return -1;
-	}
-
-	screenSurface = SDL_GetWindowSurface(window);
-
-    SDL_Surface *banciu = IMG_Load("banciu.jpg");
-
-    banciu = SDL_ConvertSurfaceFormat(banciu, screenSurface->format->format, 0);
-
-	printf("Image size:%d %d", banciu->w, banciu->h);
-
-	SDL_BlitSurface(banciu, 0, screenSurface, 0);
-
-	SDL_FreeSurface(banciu);
-
-	bool running = 1;
     SDL_Event event;
 
-    SDL_Surface *surfacefont = 0;
+    bool running = 1;
 
-    TTF_Init();
+    const int desiredfps = 24;
+    int lasttime = 0;
 
-    TTF_Font *fontubuntu = 0;
-    SDL_Color fontcolor = {200, 200, 200};
+    glClearColor(1, 1, 1, 1);
 
-    fontubuntu = TTF_OpenFont("Ubuntu-R.ttf", 32);
-
-    surfacefont = TTF_RenderText_Solid(fontubuntu, "Wake up, Neo...", fontcolor);
-
-    SDL_BlitSurface(surfacefont, 0, screenSurface, 0);
-
-    SDL_FreeSurface(surfacefont);
-
-    TTF_CloseFont(fontubuntu);
-
-	SDL_UpdateWindowSurface(window);
-
-	Mix_OpenAudio(22050, MIX_DEFAULT_FORMAT, 2, 4096 );
-
-	Mix_Music *music;
-
-	music = Mix_LoadMUS("saltarello.mp3");
-
-	Timer timer1, timer2;
-
-	timer1.start();
-
-	while(running){
-        timer2.start();
+    while(running){
+        lasttime = SDL_GetTicks();
 
         while(SDL_PollEvent(&event)){
             switch(event.type){
                 case SDL_QUIT:
                     running = 0;
-                    break;
-                case SDL_WINDOWEVENT_RESIZED:
-
                     break;
                 case SDL_KEYUP:
                     switch(event.key.keysym.sym){
@@ -133,20 +44,30 @@ int main(int argc, char ** argv) {
             }
         }
 
-        if(!Mix_PlayingMusic())
-          Mix_PlayMusic(music, 1);
+        int x = SW / 2;
+        int y = SH / 2;
 
-        int ticks = timer2.getTime();
+        glClear(GL_COLOR_BUFFER_BIT);
+        glBegin(GL_TRIANGLES);
+            glColor3b(1, 0, 0);
+            glVertex2d(200, 200);
+            glColor3b(0, 1, 0);
+            glVertex2d(300, 300);
+            glColor3b(0, 0, 1);
+            glVertex2d(200, 400);
+        glEnd();
 
-        if(ticks < FramesPerSecond)
-            SDL_Delay(FramesPerSecond - ticks);
-	}
+        SDL_GL_SwapWindow(window);
 
-	SDL_DestroyWindow(window);
-	TTF_Quit();
-	Mix_FreeMusic(music);
-	Mix_Quit();
-	SDL_Quit();
+        int time = SDL_GetTicks() - lasttime;
+        if(time < desiredfps){
+            printf("Waited :%d miliseconds\n", desiredfps - time);
+            SDL_Delay(desiredfps - time);
+        }
+    }
 
-	return 0;
+    SDL_GL_DeleteContext(context);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
 }
